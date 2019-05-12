eab2b4b7f871752cd68387078b01d7e81a9857c1
Simon Hampton <hotbelgo@gmail.com>
2019-05-12 11:36:05 +0200

    handle multiple sockets

--- src/WebSocket.elm
+++ src/WebSocket.elm
@@ -1,4 +1,4 @@
-port module WebSocket exposing (Msg, State, init, listen, send, update)
+port module WebSocket exposing (Msg, State, init, listen, send, setSockets, update)
 
 import Dict exposing (Dict)
 import Json.Decode as Decode exposing (Decoder, Value)
@@ -23,33 +23,37 @@ port fromJs : (PortMsg -> msg) -> Sub msg
 --
 
 
-listen : String -> (String -> msg) -> (Msg -> msg) -> Sub msg
-listen url lmsg wrapper =
+listen : (Msg -> msg) -> State msg -> Sub msg
+listen wrapper (State { sockets }) =
     let
         fn pmsg =
-            if pmsg.tag == url then
-                case Decode.decodeValue Decode.string pmsg.payload of
-                    Ok str ->
-                        lmsg str
+            case Dict.get pmsg.tag sockets of
+                Just (Connected lmsg _) ->
+                    case Decode.decodeValue Decode.string pmsg.payload of
+                        Ok str ->
+                            lmsg str
 
-                    Err err ->
-                        Debug.todo <| Decode.errorToString err
+                        Err err ->
+                            Debug.todo <| Decode.errorToString err
 
-            else
-                wrapper <| toMsg pmsg.tag pmsg.payload
+                Just other ->
+                    Debug.todo <| Debug.toString other
+
+                Nothing ->
+                    wrapper <| toMsg pmsg.tag pmsg.payload
     in
     fromJs fn
 
 
-send : String -> String -> State -> ( State, Cmd msg )
+send : String -> String -> State msg -> ( State msg, Cmd msg )
 send url message (State model) =
     case Dict.get url model.sockets of
-        Just (Connected socket) ->
+        Just (Connected _ socket) ->
             ( State model
             , mkSend socket url message
             )
 
-        Just (Opening _) ->
+        Just (Opening _ _) ->
             ( State { model | queues = Dict.insert url [ message ] model.queues }, Cmd.none )
 
         Nothing ->
@@ -62,8 +66,8 @@ send url message (State model) =
 --
 
 
-type State
-    = State Model
+type State msg
+    = State (Model msg)
 
 
 init =
@@ -73,21 +77,50 @@ init =
         }
 
 
-type alias Model =
-    { sockets : SocketsDict
+setSockets : List ( String, String -> msg ) -> State msg -> ( State msg, Cmd msg )
+setSockets urls (State model) =
+    let
+        handleUrl ( url, constructor ) ( accSockets, accCmds ) =
+            case Dict.get url accSockets of
+                Just _ ->
+                    ( accSockets, accCmds )
+
+                Nothing ->
+                    ( Dict.insert url (Opening constructor 0) accSockets, toJs { tag = "open", payload = Encode.string url } :: accCmds )
+
+        ( sockets, addCmds ) =
+            List.foldl handleUrl ( model.sockets, [] ) urls
+
+        closeCmds =
+            let
+                go _ v acc =
+                    case v of
+                        Connected _ socket ->
+                            toJs { tag = "close", payload = Encode.object [ ( "socket", socket ) ] } :: acc
+
+                        _ ->
+                            acc
+            in
+            List.foldl (\( k, _ ) acc -> Dict.remove k acc) model.sockets urls
+                |> Dict.foldl go []
+    in
+    ( State { model | sockets = sockets }, Cmd.batch <| closeCmds ++ addCmds )
+
+
+type alias Model msg =
+    { sockets : SocketsDict msg
     , queues : QueuesDict
     }
 
 
-type alias SocketsDict =
-    Dict String Connection
+type alias SocketsDict msg =
+    Dict String (Connection msg)
 
 
-type
-    Connection
-    --    = Opening Int Process.Id -- backoff, backoffID
-    = Opening Int
-    | Connected WS.WebSocket
+type Connection msg
+    = Opening (String -> msg) Int
+      --    = Opening Int Process.Id -- backoff, backoffID
+    | Connected (String -> msg) WS.WebSocket
 
 
 type alias QueuesDict =
@@ -99,42 +132,91 @@ type alias QueuesDict =
 
 
 type Msg
-    = Receive String Value
-    | Open String -- url
-    | Die String
-    | GoodOpen String WS.WebSocket
-    | BadOpen String
+    = GoodOpen String WS.WebSocket
     | ConfirmSend String -- url
+    | BadOpen String
+    | SocketClose CloseConfirmation
     | WSError String
 
 
-update : Msg -> State -> ( State, Cmd Msg )
+
+--    Receive String Value
+--    | Open String -- url
+--    | | Die String
+
+
+update : Msg -> State msg -> ( State msg, Cmd Msg )
 update msg (State model) =
     update_ msg model |> Tuple.mapFirst State
 
 
-update_ : Msg -> Model -> ( Model, Cmd Msg )
+update_ : Msg -> Model msg -> ( Model msg, Cmd Msg )
 update_ msg model =
     case msg of
         GoodOpen url socket ->
-            ( { model | sockets = Dict.insert url (Connected socket) model.sockets }
-            , model.queues
+            case Dict.get url model.sockets of
+                Just (Opening constructor _) ->
+                    ( { model | sockets = Dict.insert url (Connected constructor socket) model.sockets }
+                    , model.queues
+                        |> getNextForUrl url
+                        |> Maybe.map (mkSend socket url)
+                        |> Maybe.withDefault Cmd.none
+                    )
+
+                _ ->
+                    Debug.todo "GoodOpen"
+
+        BadOpen val ->
+            let
+                _ =
+                    Debug.log "BadOpen" val
+            in
+            ( model, Cmd.none )
+
+        SocketClose { url } ->
+            ( { model | sockets = Dict.remove url model.sockets }, Cmd.none )
+
+        ConfirmSend url ->
+            let
+                newModel =
+                    case Dict.get url model.queues of
+                        Just (_ :: tl) ->
+                            { model | queues = Dict.insert url tl model.queues }
+
+                        _ ->
+                            model
+            in
+            ( newModel
+            , newModel.queues
                 |> getNextForUrl url
-                |> Maybe.map (mkSend socket url)
+                |> Maybe.map (mkSendCmd newModel.sockets url)
                 |> Maybe.withDefault Cmd.none
             )
 
-        Open url ->
-            ( { model | sockets = Dict.insert url (Opening 0) model.sockets }, Cmd.none )
-
         _ ->
+            let
+                _ =
+                    Debug.log "unhandled" msg
+            in
             ( model, Cmd.none )
 
 
+getNextForUrl : String -> QueuesDict -> Maybe String
 getNextForUrl url queues =
     Dict.get url queues |> Maybe.andThen List.head
 
 
+mkSendCmd : SocketsDict msg -> String -> String -> Cmd Msg
+mkSendCmd sockets url message =
+    case Dict.get url sockets of
+        Just (Connected _ socket) ->
+            mkSend socket url message
+
+        _ ->
+            Cmd.none
+
+
+mkSend : WS.WebSocket -> String -> String -> Cmd msg
 mkSend socket url message =
     toJs
         { tag = "send"
@@ -149,6 +231,22 @@ mkSend socket url message =
 
 toMsg tag payload =
     case tag of
+        "GoodOpen" ->
+            case Decode.decodeValue decodeOpenSuccess payload of
+                Ok ( url, socket ) ->
+                    GoodOpen url socket
+
+                Err err ->
+                    WSError <| Decode.errorToString err
+
+        "BadOpen" ->
+            case Decode.decodeValue Decode.string payload of
+                Ok message ->
+                    BadOpen message
+
+                Err err ->
+                    WSError <| Decode.errorToString err
+
         "ConfirmSend" ->
             case Decode.decodeValue decodeUrl payload of
                 Ok url ->
@@ -157,16 +255,29 @@ toMsg tag payload =
                 Err err ->
                     WSError <| Decode.errorToString err
 
-        "ConfirmOpen" ->
-            case Decode.decodeValue decodeOpenSuccess payload of
-                Ok ( url, socket ) ->
-                    GoodOpen url socket
-
-                Err err ->
-                    WSError <| Decode.errorToString err
+        "SocketClose" ->
+            Decode.decodeValue decodeClose payload
+                |> Result.map SocketClose
+                |> recover (Decode.errorToString >> WSError)
 
         _ ->
             Debug.todo tag
+
+
+type alias CloseConfirmation =
+    { url : String
+    , code : Int
+    , reason : String
+    , wasClean : Bool
+    }
+
+
+decodeClose =
+    Decode.map4 CloseConfirmation
+        (Decode.field "url" Decode.string)
+        (Decode.field "code" Decode.int)
+        (Decode.field "reason" Decode.string)
+        (Decode.field "wasClean" Decode.bool)
 
 
 decodeOpenSuccess : Decoder ( String, WS.WebSocket )
@@ -178,3 +289,13 @@ decodeOpenSuccess =
 
 decodeUrl =
     Decode.field "url" Decode.string
+
+
+recover : (a -> b) -> Result a b -> b
+recover fn res =
+    case res of
+        Ok b ->
+            b
+
+        Err a ->
+            fn a
