91869714af95187e32d1b3daa993e7690daa7cf0
Simon Hampton <hotbelgo@gmail.com>
2019-05-25 08:26:59 +0200

    add tests for real

---
+++ tests/Example.elm
@@ -0,0 +1,37 @@
+module WebSocketTests exposing (sendTests, setSocketsTests)
+
+import Dict
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Test exposing (..)
+import WebSocket exposing (..)
+
+
+setSocketsTests : Test
+setSocketsTests =
+    describe "setSockets Tests"
+        [ test "attempt to open new socket" <|
+            \_ ->
+                setSockets_ [ ws1 ] (initModel (\_ -> Cmd.none))
+                    |> Tuple.first
+                    |> .sockets
+                    |> Dict.get ws1
+                    |> Expect.equal (Just <| Opening 0)
+        ]
+
+
+sendTests : Test
+sendTests =
+    describe "send Tests"
+        [ test "send when no socket ready adds to queue" <|
+            \_ ->
+                send_ ws1 "test" (initModel (\_ -> Cmd.none))
+                    |> Tuple.first
+                    |> .queues
+                    |> Dict.get ws1
+                    |> Expect.equal (Just [ "test" ])
+        ]
+
+
+ws1 =
+    "ws://test1"
